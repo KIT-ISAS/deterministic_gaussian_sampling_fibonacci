9abaf89029126e0b8f628b7ae8ed2a32cb5089fe
Vlad Korsakov <ulqba@student.kit.edu>
2025-12-12 21:14:30 +0100

    added tests

---
+++ tests/sample_gaus_test.py
@@ -0,0 +1,27 @@
+from deterministic_gaussian_sampling_fibonacci import sample_gaussian_fibonacci
+import pytest
+import numpy as np
+
+FIB_TYPES = ['ImprovedFrolov', 'ClassicalFrolov', 'Fibonacci']
+FIB_DIMS = [2, 3, 4, 5, 6]
+
+
+@pytest.mark.parametrize("type", FIB_TYPES)
+@pytest.mark.parametrize("dim", FIB_DIMS)
+def test_001_gaus_sample_test(type, dim):
+	if dim == 6:
+		sampcount = 1000 # dim = 6, LVol = 10000, type = 'ClassicalFrolov' is unsupported
+	else:
+		sampcount = 10000
+	np.random.seed(0)
+	mu = np.random.randn(dim)
+	A = np.random.randn(dim, dim)
+	cov = A @ A.T  # Ensures positive semi-definite covariance
+	samp = sample_gaussian_fibonacci(mu, cov, sampcount, type)
+
+	mu_pred = np.mean(samp, axis=0) # shape (3,)
+	C_pred = np.cov(samp, rowvar=False, bias=True)
+
+	assert np.all(np.isclose(mu, mu_pred, 10**-2))
+	if sampcount > 10000:
+		assert np.all(np.isclose(cov, C_pred, 10**-2))
